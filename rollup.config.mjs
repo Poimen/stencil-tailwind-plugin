16694dbca420a2b499349b0d8a014f8100cbe305
Richard Shephard <richard.shephard@gmail.com>
2025-04-03 18:59:29 +0200

    move back to rollup

---
+++ rollup.config.mjs
@@ -0,0 +1,45 @@
+import terser from '@rollup/plugin-terser';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
+import pkg from './package.json' with {type: 'json'};
+
+const moduleName = pkg.name.replace(/^@.*\//, '');
+const inputFileName = 'src/index.ts';
+const author = pkg.author;
+const banner = `
+  /**
+   * @license
+   * author: ${author}
+   * ${moduleName}.js v${pkg.version}
+   * Released under the ${pkg.license} license.
+   */
+`;
+
+export default {
+  input: inputFileName,
+  output: [{
+    file: pkg.main,
+    format: 'cjs', // commonJS
+    sourcemap: true,
+    exports: 'named',
+    banner,
+    plugins: [
+      terser(),
+    ],
+  }, {
+    file: pkg.module,
+    format: 'esm', // ES Modules
+    sourcemap: true,
+    exports: 'named',
+    banner,
+    plugins: [
+      terser(),
+    ],
+  }],
+  plugins: [
+    peerDepsExternal(),
+    commonjs(),
+    typescript(),
+  ],
+};
